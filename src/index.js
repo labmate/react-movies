6070acaf0247be0ea01afea7d72a9278cd2da655
Labundy Mate <labundym@gmail.com>
2018-12-09 23:03:03 +0100

    task#3 added components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './app/app.js';
+import App from './containers/app.js';
+import './reset.less';
 
 render(
   <App />,
